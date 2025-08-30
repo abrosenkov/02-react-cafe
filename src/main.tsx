22a4f6d89514f10f6fd325460a20e3e74b90e363
abrosenkov <abrosenkov91@gmail.com>
2025-08-30 14:29:01 +0200

    add state

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+import "modern-normalize";
+
+import App from './components/App/App';
+
+// Глобальні стилі (додатково)
+// import "./global.css";
+
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
